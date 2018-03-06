d4aa3551a306d0dab03787db31999515cb4a0823
Tareq Mohammad Yousuf <tareq.y@gmail.com>
2018-03-06 19:11:58 +0600

    tried center layout, navigation and a fixed prayer.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,35 +6,52 @@ import ReactDOM from 'react-dom';
 import './index.css';
 // end::vars[]
 
-// tag::app[]
-function App(props) {
+function Navigate(props) {
     return (
-        <div className="body">
-            <div className="header">
+        <button className="navigate">
+            {props.value}
+        </button>
+    );
+}
+
+function Prayer(props) {
+    return (
+        <div className="prayer">
+            {props.value}
+        </div>
+    );
+}
+
+// tag::content[]
+class Content extends React.Component {
+    
+    render() {
+        return (
+            <div className="content">
+                <Navigate value="previous"></Navigate>
+                <Prayer value="O our Sustainer! Give us the good of this world and the good of the hereafter, and save us from the punishment of the fire."></Prayer>
+                <Navigate value="next"></Navigate>
+            </div>
+        );
+    }
+}
+// end::content[]
+
+// tag::app[]
+function MunjateMaqbool(props) {
+    return (
+        <div className="mem">
+            <div className="title">
                 <h1>Munajat E Maqbool!</h1>
             </div>
-            <TodoList name="moi!"></TodoList>
+            <div>
+                <Content></Content>
+            </div>
         </div>
     );
 }
 // end::app[]
 
-class TodoList extends React.Component {
-    render() {
-        return (
-            <div className="todo-list">
-                <h3>todo liste pour {this.props.name}</h3>
-                <ul>
-                    <li>memapp</li>
-                    <li>memappclient</li>
-                    <li>telefonix</li>
-                    <li>noticeboard</li>
-                </ul>
-            </div>
-        );
-    }
-}
-
 // tag::render[]
-ReactDOM.render(<App />, document.getElementById('react'));
+ReactDOM.render(<MunjateMaqbool />, document.getElementById('react'));
 // end::render[]
