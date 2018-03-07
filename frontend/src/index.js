e6597b7980cda21e24398d45e8c306cbf39be872
Tareq Mohammad Yousuf <tareq.y@gmail.com>
2018-03-07 15:52:37 +0600

    make next and previous increase and decrease content value by 1

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,42 +6,68 @@ import ReactDOM from 'react-dom';
 import './index.css';
 // end::vars[]
 
-function Navigate(props) {
-    return (
-        <button className="navigate">
-            {props.value}
-        </button>
-    );
-}
-
-function Prayer(props) {
-    return (
-        <div className="prayer">
-            {props.value}
-        </div>
-    );
-}
-
 // tag::app[]
-function MunjateMaqbool(props) {
-    return (
-        <div className="mem">
-            <div className="title">
-                <h1>Munajat E Maqbool!</h1>
+class MunjateMaqbool extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: 0,
+            first: true,
+            last: false,
+        };
+    }
+
+    next() {
+        var next = this.state.value + 1;
+        this.setState({
+            value: next,
+            first: next === 0,
+            last: next === 10,
+        });
+    }
+
+    previous() {
+        var previous = this.state.value - 1;
+        this.setState({
+            value: previous,
+            first: previous === 0,
+            last: previous === 10,
+        });
+    }
+
+    render() {
+        return (
+            <div className="mem">
+                <div className="title">
+                    <h1>Munajat E Maqbool!</h1>
+                </div>
+
+                <table className="content">
+                    <tr>
+                        <td>
+                            <button className="navigate" disabled={this.state.first}
+                                onClick={() => this.previous()}>
+                                previous
+                            </button>
+                        </td>
+                        <td>
+                            <div className="prayer">
+                                {this.state.value}
+                            </div>
+                        </td>
+                        <td>
+                            <button className="navigate" disabled={this.state.last}
+                                onClick={() => this.next()}>
+                                next
+                            </button>
+                        </td>
+                    </tr>
+                </table>
+
             </div>
-
-            <table className="content">
-                <tr>
-                    <td><Navigate value="<<" class="previous"></Navigate></td>
-                    <td>
-                        <Prayer value="O our Sustainer! Open the gates of patience upon us and cause us to die as Muslims."></Prayer>
-                    </td>
-                    <td><Navigate value=">>"></Navigate></td>
-                </tr>
-            </table>
-
-        </div>
-    );
+        );
+    }
 }
 // end::app[]
 
